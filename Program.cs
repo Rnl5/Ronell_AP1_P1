8add0bce0db8a394c448336273f857bf1374ec4c
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-09-25 20:18:07 -0400

    Agregando NotificationService

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Ronell_AP1_P1.DAL;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ builder.Services.AddServerSideBlazor();
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
 builder.Services.AddDbContext<Context>(op => op.UseSqlite());
+
+builder.Services.AddScoped<NotificationService>();
 
 var app = builder.Build();
 
