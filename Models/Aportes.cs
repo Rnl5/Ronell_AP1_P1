16e0b3780ea3d55766126caf3a4f31da8c7b8f78
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-02 19:41:25 -0400

    Creando el modelo Aportes

---
+++ Models/Aportes.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Aportes
+{
+    [Key]
+    public int AporteId {get; set;}
+
+    [Required(ErrorMessage ="Es obligatorio insertar el nombre completo de la persona en este campo")]
+    [RegularExpression("@[a-zA-Z]$")]
+    public string? Persona {get; set;}
+
+    [Required(ErrorMessage ="Debe de insertar una fecha")]
+    public DateTime? Fecha {get; set;} = DateTime.Now;
+
+    [Required(ErrorMessage ="Es obligatorio insertar una observacion")]
+    [MinLength(5, ErrorMessage ="Debe de contener minimo {1} caracteres")]
+    [MaxLength(100, ErrorMessage ="Debe de contener maximo {1} caracteres")]
+    public string? Observacion {get; set;}
+
+    [Required(ErrorMessage ="Debe de insertar un monto")]
+    public double? Monto {get; set;}
+}
